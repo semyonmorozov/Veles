515f183b90e10b5f06433a3c12636127c0b5d384
����� <semyonmorozov@gmail.com>
2022-07-03 16:45:30 +0500

    Во славу Велеса

---
+++ Assets/Scripts/GlobalEventManager.cs
@@ -0,0 +1,6 @@
+using UnityEngine.Events;
+
+public class GlobalEventManager
+{
+    public static UnityEvent PlayerFell = new UnityEvent();
+}
---
+++ Assets/Scripts/Player/FallEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class FallEventHandler : MonoBehaviour
+    {
+        private Material material;
+
+        private void Awake()
+        {
+            material = GetComponent<MeshRenderer>().material;
+            GlobalEventManager.PlayerFell.AddListener(Handle);
+        }
+
+        private void Handle()
+        {
+            material.color = Color.black;
+        }
+    }
+}
