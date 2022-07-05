edf953ec58b730a9b1d1e4d68933b319ddb1c167
Семен <semyonmorozov@gmail.com>
2022-07-05 10:02:35 +0500

    Поправил поворот игрока к мыши, передвижение. Поправил поведение снарядов

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -5,7 +5,7 @@ public class Shell : MonoBehaviour
 {
     public int moveSpeed = 200;
     public int lifeTimeSeconds = 2;
-    private Rigidbody rigidbody;
+    private new Rigidbody rigidbody;
 
     private void Awake()
     {
@@ -15,27 +15,13 @@ public class Shell : MonoBehaviour
     private void Start ()
     {
         Destroy (gameObject, lifeTimeSeconds);
-        
-        
-        //Get the Screen positions of the object
-        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
-         
-        //Get the Screen position of the mouse
-        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-         
-        //Get the angle between the points
-        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
- 
-        //Ta Daaa
-        transform.rotation =  Quaternion.Euler (new Vector3(0f,-angle, 0f));
-    }
- 
-    float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
     
     private void FixedUpdate()
     {
-        rigidbody.velocity = transform.TransformDirection(transform.forward * (moveSpeed * Time.fixedDeltaTime));
+        rigidbody.velocity = transform.forward * (moveSpeed * Time.fixedDeltaTime);
+        
+        var forward = transform.TransformDirection(Vector3.forward) * 1;
+        Debug.DrawRay(transform.position, forward, Color.red);
     }
 }
--- Assets/Scripts/Unit/Player/Controller.cs
+++ Assets/Scripts/Unit/Player/Controller.cs
@@ -16,17 +16,20 @@ namespace Unit.Player
 
         public float speed = 1;
         public float rotationSpeed = 100;
+        public LayerMask groundMask;
 
         public ControllerState state = ControllerState.ExploreWorld;
 
         public Rigidbody shell;
         
         private new Rigidbody rigidbody;
+        private new Camera camera;
 
         private void Awake()
         {
             rigidbody = GetComponent<Rigidbody>();
-            
+            camera = Camera.main;
+
             //TODO отписаться от события смерти, когда появится меню, должно быть реализовано иначе
             GlobalEventManager.PlayerDeath.AddListener(() => state = ControllerState.InMenu); 
         }
@@ -39,15 +42,35 @@ namespace Unit.Player
                     break;
                 case ControllerState.ExploreWorld:
                     MovePlayer();
-                    //RotateToCursor();
+                    RotateToCursor();
+                    DrawPlayerForwardRay();
                     break;
             }
         }
 
         private void RotateToCursor()
         {
-            var lookRotation = Quaternion.LookRotation(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+            var mousePos = GetMousePosition().position;
+            var lookRotation = Quaternion.LookRotation(mousePos - transform.position);
+            lookRotation.z = 0;
+            lookRotation.x = 0;
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.fixedDeltaTime);
+        }
+
+        private void DrawPlayerForwardRay()
+        {
+            var playerTransform = transform;
+            var forward = playerTransform.TransformDirection(Vector3.forward) * 10;
+            Debug.DrawRay(playerTransform.position, forward, Color.green);
+        }
+
+        private (bool success, Vector3 position) GetMousePosition()
+        {
+            var ray = camera.ScreenPointToRay(Input.mousePosition);
+ 
+            return Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, groundMask) 
+                ? (success: true, position: hitInfo.point) 
+                : (success: false, position: Vector3.zero);
         }
 
         private void Update()
@@ -75,8 +98,8 @@ namespace Unit.Player
         {
             var horizontalDelta = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
             var verticalDelta = Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime;
-            var velocityY = rigidbody.velocity.y;
-            rigidbody.velocity = transform.TransformDirection(new Vector3(verticalDelta, velocityY, -horizontalDelta));
+            
+            rigidbody.velocity = new Vector3(verticalDelta, 0, -horizontalDelta);
         }
     }
 }
