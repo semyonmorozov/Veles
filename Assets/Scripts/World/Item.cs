b228af0b5ea0d771ba7cdf2935a2499a9d6e82db
Семен <semyonmorozov@gmail.com>
2022-07-10 18:23:12 +0500

    первый айтем

---
+++ Assets/Scripts/World/Item.cs
@@ -0,0 +1,18 @@
+﻿using Units.Player;
+using UnityEngine;
+
+namespace World
+{
+    public class Item : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            var collisionGameObject = other.gameObject;
+            if (collisionGameObject.CompareTag("Player"))
+            {
+                collisionGameObject.GetComponent<PlayerStats>().Agility = 1;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
